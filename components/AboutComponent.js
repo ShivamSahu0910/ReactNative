80a8a42d1d17bf45c04d5e128bc921b55ef342da
Shivam Sahu <shivamsahu997@gmail.com>
2020-12-04 10:26:12 +0530

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem, Avatar } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return{
@@ -59,22 +60,45 @@ const mapStateToProps = state => {
                 </ListItem>
             );
         }
-        return(
-            <ScrollView>
-                <History />
-                <Card>
-                    <Card.Title>Corporate Leadership</Card.Title>
-                <Card.Divider />
-                <FlatList
-                    data={this.props.leaders.leaders}
-                    renderItem={renderLeaderItem}
-                    keyExtractor={item => item.id.toString()} 
-                />
-                </Card>
-            </ScrollView>
-        )
+        if (this.props.leaders.isLoading) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else if (this.props.leaders.errMess) {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card
+                        title='Corporate Leadership'>
+                        <Text>{this.props.leaders.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        else {
+            return(
+                <ScrollView>
+                    <History />
+                    <Card>
+                        <Card.Title>Corporate Leadership</Card.Title>
+                    <Card.Divider />
+                    <FlatList
+                        data={this.props.leaders.leaders}
+                        renderItem={renderLeaderItem}
+                        keyExtractor={item => item.id.toString()} 
+                    />
+                    </Card>
+                </ScrollView>
+            )
+        }
     }
-
 }
 
 export default connect(mapStateToProps)(About);
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -3,6 +3,7 @@ import { FlatList } from 'react-native';
 import { Tile } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
 
 const mapStateToProps = state => {
     return{
@@ -28,15 +29,29 @@ class Menu extends Component {
                     imageSrc={{ uri: baseUrl + item.image }}
                 />
             );
-        }        
-
-        return (
-            <FlatList 
-                data={this.props.dishes.dishes}
-                renderItem={renderMenuItem}
-                keyExtractor={item => item.id.toString()}
-                />
-        );
+        }
+        
+        if (this.props.dishes.isLoading) {
+            return(
+                <Loading />
+            );
+        }
+        else if (this.props.dishes.errMess) {
+            return(
+                <View>            
+                    <Text>{props.dishes.errMess}</Text>
+                </View>            
+            );
+        }
+        else{
+            return ( 
+                <FlatList 
+                    data={this.props.dishes.dishes}
+                    renderItem={renderMenuItem}
+                    keyExtractor={item => item.id.toString()}
+                    />
+            );
+        }       
     }
 }
 
